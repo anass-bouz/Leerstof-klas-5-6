d86c9269eda6bfd7bf787482048e61afb53121f3
anass-bouz <anass.bouzaya@student.odisee.be>
2024-05-27 14:34:02 +0200

    sef

--- Les 3/Demos/PetRock/src/main/java/be/odisee/PetRock.java
+++ Les 3/Demos/PetRock/src/main/java/be/odisee/PetRock.java
@@ -3,22 +3,22 @@ package be.odisee;
 public class PetRock {
 
     private String name;
-    private boolean happy;
 
     public PetRock(String name) {
         this.name = name;
-        this.happy = false;
-    }
 
+    }
     public String getName() {
         return name;
     }
 
-    public boolean IsHappy(){
-        return happy;
+    class Apparaat {
+        private boolean aan;
+        public boolean isAan() { return aan;}
+        public boolean toggleAanUit() { aan=!aan; return isAan(); }
+    }
+    public class aanUitTest {
+        private Apparaat wasmachine=new Apparaat();
     }
 
-    public void play(){
-        this.happy = true;
-    }
 }
--- Les 3/Demos/PetRock/src/test/java/be/odisee/PetRockTest.java
+++ Les 3/Demos/PetRock/src/test/java/be/odisee/PetRockTest.java
@@ -6,30 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PetRockTest {
 
-    @Test
+    @org.junit.jupiter.api.Test
     void getName() {
         PetRock rocky = new PetRock("Rocky");
         assertEquals("Rocky", rocky.getName());
     }
 
-    @Test
-    void isHappy_AtStart_ReturnsFalse(){
-        // Given
-        PetRock rocky = new PetRock("Rocky");
 
-        // Assert
-        assertFalse(rocky.IsHappy());
-    }
-
-    @Test
-    void isHappy_AfterPlay_ReturnsTrue(){
-        // Given
-        PetRock rocky = new PetRock("Rocky");
-
-        // Act
-        rocky.play();
-
-        // Assert
-        assertTrue(rocky.IsHappy());
-    }
 }
